353a447e987c64089fef63075c1de3a731849f95
笹沢裕一 <sasazawa@sasazawayuuichinoMacBook-Pro.local>
2018-11-23 18:35:10 +0900

    add

--- qgevalcap/eval.py
+++ qgevalcap/eval.py
@@ -99,9 +99,9 @@ def eval(out_file, src_file, tgt_file, isDIn = False, num_pairs = 500):
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("-out", "--out_file", dest="out_file", default="./output/pred.txt", help="output file to compare")
-    parser.add_argument("-src", "--src_file", dest="src_file", default="../data/processed/src-test.txt", help="src file")
-    parser.add_argument("-tgt", "--tgt_file", dest="tgt_file", default="../data/processed/tgt-test.txt", help="target file")
+    parser.add_argument("-out", "--out_file", dest="out_file", default="../sentence/pred.txt", help="output file to compare")
+    parser.add_argument("-src", "--src_file", dest="src_file", default="../data/processed/src-dev.txt", help="src file")
+    parser.add_argument("-tgt", "--tgt_file", dest="tgt_file", default="../data/processed/tgt-dev.txt", help="target file")
     args = parser.parse_args()
 
     print "scores: \n"
